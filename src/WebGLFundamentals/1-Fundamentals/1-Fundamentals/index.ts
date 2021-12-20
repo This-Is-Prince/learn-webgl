4c1c99143789d9abe4ed3ed43b2fb16195c41ef6
Prince <this.is.kumar.prince@gmail.com>
2021-12-20 20:00:59 +0530

    basic triangle draw completed

--- src/WebGLFundamentals/1-Fundamentals/1-Fundamentals/index.ts
+++ src/WebGLFundamentals/1-Fundamentals/1-Fundamentals/index.ts
@@ -7,11 +7,16 @@ const canvas = document.getElementById("canvas") as HTMLCanvasElement;
  * WebGLRenderingContext
  */
 const gl = canvas.getContext("webgl");
+
+/**
+ * Create Shader
+ */
 function createShader(gl: WebGLRenderingContext, type: number, source: string) {
   const shader = gl.createShader(type) as WebGLShader;
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
   const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+
   if (success) {
     return shader;
   }
@@ -19,6 +24,9 @@ function createShader(gl: WebGLRenderingContext, type: number, source: string) {
   gl.deleteShader(shader);
 }
 
+/**
+ * Create Program
+ */
 function createProgram(
   gl: WebGLRenderingContext,
   vertexShader: WebGLShader,
@@ -53,10 +61,52 @@ if (gl !== null) {
     gl.FRAGMENT_SHADER,
     fragmentShaderSource
   ) as WebGLShader;
+
   const program = createProgram(
     gl,
     vertexShader,
     fragmentShader
   ) as WebGLProgram;
+
   const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+
+  const positionBuffer = gl.createBuffer();
+
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  // three 2d points
+  const positions = [0, 0, 0, 0.5, 0.7, 0];
+  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+  // Clear the canvas
+  gl.clearColor(0, 0, 0, 0);
+  gl.clear(gl.COLOR_BUFFER_BIT);
+  gl.useProgram(program);
+  gl.enableVertexAttribArray(positionAttributeLocation);
+  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+  const size = 2;
+  const type = gl.FLOAT;
+  const normalize = false;
+  const stride = 0;
+  let offset = 0;
+  gl.vertexAttribPointer(
+    positionAttributeLocation,
+    size,
+    type,
+    normalize,
+    stride,
+    offset
+  );
+
+  // draw
+  const primitiveType = gl.TRIANGLES;
+  offset = 0;
+  const count = 3;
+  gl.drawArrays(primitiveType, offset, count);
+} else {
+  alert("WebGL not supported in your browser");
+  throw new Error("WebGL not supported in your browser");
 }
