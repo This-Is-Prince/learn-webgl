c8fc247a5f5a98ce6f386d97eefdd8fe02c4ea38
Prince <this.is.kumar.prince@gmail.com>
2021-12-21 17:20:58 +0530

    canvas resize

--- src/WebGLFundamentals/1-Fundamentals/1-Fundamentals/index.ts
+++ src/WebGLFundamentals/1-Fundamentals/1-Fundamentals/index.ts
@@ -11,12 +11,13 @@ const gl = canvas.getContext("webgl");
 /**
  * Create Shader
  */
+
 function createShader(gl: WebGLRenderingContext, type: number, source: string) {
   const shader = gl.createShader(type) as WebGLShader;
   gl.shaderSource(shader, source);
   gl.compileShader(shader);
-  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
 
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS) as boolean;
   if (success) {
     return shader;
   }
@@ -27,6 +28,7 @@ function createShader(gl: WebGLRenderingContext, type: number, source: string) {
 /**
  * Create Program
  */
+
 function createProgram(
   gl: WebGLRenderingContext,
   vertexShader: WebGLShader,
@@ -36,7 +38,8 @@ function createProgram(
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
   gl.linkProgram(program);
-  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS) as boolean;
   if (success) {
     return program;
   }
@@ -44,69 +47,70 @@ function createProgram(
   gl.deleteProgram(program);
 }
 
-if (gl !== null) {
-  const vertexShaderSource = (
-    document.getElementById("vertex-shader-2d") as HTMLScriptElement
-  ).text;
-  const fragmentShaderSource = (
-    document.getElementById("fragment-shader-2d") as HTMLScriptElement
-  ).text;
-  const vertexShader = createShader(
-    gl,
-    gl.VERTEX_SHADER,
-    vertexShaderSource
-  ) as WebGLShader;
-  const fragmentShader = createShader(
-    gl,
-    gl.FRAGMENT_SHADER,
-    fragmentShaderSource
-  ) as WebGLShader;
-
-  const program = createProgram(
-    gl,
-    vertexShader,
-    fragmentShader
-  ) as WebGLProgram;
-
-  const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
-
-  const positionBuffer = gl.createBuffer();
-
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  // three 2d points
-  const positions = [0, 0, 0, 0.5, 0.7, 0];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
-
-  gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
-
-  // Clear the canvas
-  gl.clearColor(0, 0, 0, 0);
-  gl.clear(gl.COLOR_BUFFER_BIT);
-  gl.useProgram(program);
-  gl.enableVertexAttribArray(positionAttributeLocation);
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-
-  const size = 2;
-  const type = gl.FLOAT;
-  const normalize = false;
-  const stride = 0;
-  let offset = 0;
-  gl.vertexAttribPointer(
-    positionAttributeLocation,
-    size,
-    type,
-    normalize,
-    stride,
-    offset
-  );
-
-  // draw
-  const primitiveType = gl.TRIANGLES;
-  offset = 0;
-  const count = 3;
-  gl.drawArrays(primitiveType, offset, count);
-} else {
+if (gl === null) {
   alert("WebGL not supported in your browser");
   throw new Error("WebGL not supported in your browser");
 }
+
+// Clear the canvas
+gl.clearColor(0, 0, 0, 0);
+gl.clear(gl.COLOR_BUFFER_BIT);
+
+const vertexShaderSource = (
+  document.getElementById("vertex-shader-2d") as HTMLScriptElement
+).text;
+const fragmentShaderSource = (
+  document.getElementById("fragment-shader-2d") as HTMLScriptElement
+).text;
+
+const vertexShader = createShader(
+  gl,
+  gl.VERTEX_SHADER,
+  vertexShaderSource
+) as WebGLShader;
+const fragmentShader = createShader(
+  gl,
+  gl.FRAGMENT_SHADER,
+  fragmentShaderSource
+) as WebGLShader;
+
+const program = createProgram(gl, vertexShader, fragmentShader) as WebGLProgram;
+gl.useProgram(program);
+
+const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
+gl.enableVertexAttribArray(positionAttributeLocation);
+
+let { clientWidth, clientHeight } = gl.canvas;
+const pixelRatio = Math.min(window.devicePixelRatio, 2);
+clientHeight = (clientHeight * pixelRatio) | 0;
+clientWidth = (clientWidth * pixelRatio) | 0;
+gl.canvas.width = clientWidth;
+gl.canvas.height = clientHeight;
+gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
+
+const positionBuffer = gl.createBuffer();
+gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+
+// three 2d points
+const positions = [0, 0, 0, 0.5, 0.7, 0];
+gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(positions), gl.STATIC_DRAW);
+
+const size = 2;
+const type = gl.FLOAT;
+const normalize = false;
+const stride = 0;
+let offset = 0;
+gl.vertexAttribPointer(
+  positionAttributeLocation,
+  size,
+  type,
+  normalize,
+  stride,
+  offset
+);
+
+// draw
+const primitiveType = gl.TRIANGLES;
+offset = 0;
+const count = 3;
+gl.drawArrays(primitiveType, offset, count);
