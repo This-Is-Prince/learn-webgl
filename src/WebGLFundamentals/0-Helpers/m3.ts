d1cfd05242e84d73f2c864f28959ff33202119b3
Prince <this.is.kumar.prince@gmail.com>
2021-12-22 21:50:39 +0530

    new type introduce in mat

--- src/WebGLFundamentals/0-Helpers/m3.ts
+++ src/WebGLFundamentals/0-Helpers/m3.ts
@@ -9,6 +9,12 @@ type Matrix3 = [
   number,
   number
 ];
+type Mat3 = [
+  [number, number, number],
+  [number, number, number],
+  [number, number, number]
+];
+
 /**
  * Multiply Function
  */
@@ -46,12 +52,53 @@ const multiply: MultiplyFunType = (a, b) => {
   ];
 };
 
+type MultiplyMatFunType = (a: Mat3, b: Mat3) => Mat3;
+const multiplyMat: MultiplyMatFunType = (a, b) => {
+  const {
+    "0": { "0": a00, "1": a01, "2": a02 },
+    "1": { "0": a10, "1": a11, "2": a12 },
+    "2": { "0": a20, "1": a21, "2": a22 },
+  } = a;
+  const {
+    "0": { "0": b00, "1": b01, "2": b02 },
+    "1": { "0": b10, "1": b11, "2": b12 },
+    "2": { "0": b20, "1": b21, "2": b22 },
+  } = b;
+
+  return [
+    [
+      b00 * a00 + b01 * a10 + b02 * a20,
+      b00 * a01 + b01 * a11 + b02 * a21,
+      b00 * a02 + b01 * a12 + b02 * a22,
+    ],
+    [
+      b10 * a00 + b11 * a10 + b12 * a20,
+      b10 * a01 + b11 * a11 + b12 * a21,
+      b10 * a02 + b11 * a12 + b12 * a22,
+    ],
+    [
+      b20 * a00 + b21 * a10 + b22 * a20,
+      b20 * a01 + b21 * a11 + b22 * a21,
+      b20 * a02 + b21 * a12 + b22 * a22,
+    ],
+  ];
+};
+
 /**
  * Identity Function
  */
 type IdentityFunType = () => Matrix3;
 const identity: IdentityFunType = () => {
   return [1, 0, 0, 0, 1, 0, 0, 0, 1];
+};
+
+type IdentityMatFunType = () => Mat3;
+const identityMat: IdentityMatFunType = () => {
+  return [
+    [1, 0, 0],
+    [0, 1, 0],
+    [0, 0, 1],
+  ];
 };
 
 /**
@@ -62,12 +109,26 @@ const projection: ProjectionFunType = (width, height) => {
   return [2 / width, 0, 0, 0, -2 / height, 0, -1, 1, 1];
 };
 
+type ProjectionMatFunType = (width: number, height: number) => Mat3;
+const projectionMat: ProjectionMatFunType = (width, height) => {
+  return [
+    [2 / width, 0, 0],
+    [0, -2 / height, 0],
+    [-1, 1, 1],
+  ];
+};
+
 /**
  * Project Function
  */
 type ProjectFunType = (m: Matrix3, width: number, height: number) => Matrix3;
 const project: ProjectFunType = (m, width, height) => {
   return multiply(m, projection(width, height));
+};
+
+type ProjectMatFunType = (m: Mat3, width: number, height: number) => Mat3;
+const projectMat: ProjectMatFunType = (m, width, height) => {
+  return multiplyMat(m, projectionMat(width, height));
 };
 
 /**
@@ -78,12 +139,26 @@ const translation: TranslationFunType = (tx, ty) => {
   return [1, 0, 0, 0, 1, 0, tx, ty, 1];
 };
 
+type TranslationMatFunType = (tx: number, ty: number) => Mat3;
+const translationMat: TranslationMatFunType = (tx, ty) => {
+  return [
+    [1, 0, 0],
+    [0, 1, 0],
+    [tx, ty, 1],
+  ];
+};
+
 /**
  * Translate Function
  */
 type TranslateFunType = (m: Matrix3, tx: number, ty: number) => Matrix3;
 const translate: TranslateFunType = (m, tx, ty) => {
   return multiply(m, translation(tx, ty));
+};
+
+type TranslateMatFunType = (m: Mat3, tx: number, ty: number) => Mat3;
+const translateMat: TranslateMatFunType = (m, tx, ty) => {
+  return multiplyMat(m, translationMat(tx, ty));
 };
 
 /**
@@ -226,6 +301,27 @@ const inverse: InverseFunType = (m) => {
     d * (m[1 * 3 + 0] * m[2 * 3 + 1] - m[1 * 3 + 1] * m[2 * 3 + 0]),
     -d * (m[0 * 3 + 0] * m[2 * 3 + 1] - m[0 * 3 + 1] * m[2 * 3 + 0]),
     d * (m[0 * 3 + 0] * m[1 * 3 + 1] - m[0 * 3 + 1] * m[1 * 3 + 0]),
+  ];
+};
+
+type InverseMatFunType = (m: Mat3) => Mat3;
+const inverseMat: InverseMatFunType = (m) => {
+  const t00 = m[2][2] * m[1][1] - m[2][1] * m[1][2];
+  const t10 = m[2][2] * m[0][1] - m[2][1] * m[0][2];
+  const t20 = m[1][2] * m[0][1] - m[1][1] * m[0][2];
+  const d = 1.0 / (m[0][0] * t00 - m[1][0] * t10 + m[2][0] * t20);
+  return [
+    [d * t00, -d * t10, d * t20],
+    [
+      -d * (m[1][0] * m[2][2] - m[1][2] * m[2][0]),
+      d * (m[0][0] * m[1][2] - m[0][2] * m[2][0]),
+      -d * (m[0][0] * m[1][2] - m[0][2] * m[1][0]),
+    ],
+    [
+      d * (m[1][0] * m[2][1] - m[1][1] * m[2][0]),
+      -d * (m[0][0] * m[2][1] - m[0][1] * m[2][0]),
+      d * (m[0][0] * m[1][1] - m[0][1] * m[1][0]),
+    ],
   ];
 };
 
