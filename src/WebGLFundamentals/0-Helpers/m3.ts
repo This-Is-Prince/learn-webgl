4c3e2ee345e62e64ef3bb1d605a97ec2588935e6
Prince <this.is.kumar.prince@gmail.com>
2021-12-22 21:16:50 +0530

    m3 library types added

--- src/WebGLFundamentals/0-Helpers/m3.ts
+++ src/WebGLFundamentals/0-Helpers/m3.ts
@@ -1,23 +1,37 @@
-type Matrix3 = number[];
-function multiply(a: Matrix3, b: Matrix3) {
-  var a00 = a[0 * 3 + 0];
-  var a01 = a[0 * 3 + 1];
-  var a02 = a[0 * 3 + 2];
-  var a10 = a[1 * 3 + 0];
-  var a11 = a[1 * 3 + 1];
-  var a12 = a[1 * 3 + 2];
-  var a20 = a[2 * 3 + 0];
-  var a21 = a[2 * 3 + 1];
-  var a22 = a[2 * 3 + 2];
-  var b00 = b[0 * 3 + 0];
-  var b01 = b[0 * 3 + 1];
-  var b02 = b[0 * 3 + 2];
-  var b10 = b[1 * 3 + 0];
-  var b11 = b[1 * 3 + 1];
-  var b12 = b[1 * 3 + 2];
-  var b20 = b[2 * 3 + 0];
-  var b21 = b[2 * 3 + 1];
-  var b22 = b[2 * 3 + 2];
+type Matrix3 = [
+  number,
+  number,
+  number,
+  number,
+  number,
+  number,
+  number,
+  number,
+  number
+];
+/**
+ * Multiply Function
+ */
+type MultiplyFunType = (a: Matrix3, b: Matrix3) => Matrix3;
+const multiply: MultiplyFunType = (a, b) => {
+  const a00 = a[0 * 3 + 0];
+  const a01 = a[0 * 3 + 1];
+  const a02 = a[0 * 3 + 2];
+  const a10 = a[1 * 3 + 0];
+  const a11 = a[1 * 3 + 1];
+  const a12 = a[1 * 3 + 2];
+  const a20 = a[2 * 3 + 0];
+  const a21 = a[2 * 3 + 1];
+  const a22 = a[2 * 3 + 2];
+  const b00 = b[0 * 3 + 0];
+  const b01 = b[0 * 3 + 1];
+  const b02 = b[0 * 3 + 2];
+  const b10 = b[1 * 3 + 0];
+  const b11 = b[1 * 3 + 1];
+  const b12 = b[1 * 3 + 2];
+  const b20 = b[2 * 3 + 0];
+  const b21 = b[2 * 3 + 1];
+  const b22 = b[2 * 3 + 2];
 
   return [
     b00 * a00 + b01 * a10 + b02 * a20,
@@ -30,83 +44,159 @@ function multiply(a: Matrix3, b: Matrix3) {
     b20 * a01 + b21 * a11 + b22 * a21,
     b20 * a02 + b21 * a12 + b22 * a22,
   ];
-}
+};
 
-function identity() {
+/**
+ * Identity Function
+ */
+type IdentityFunType = () => Matrix3;
+const identity: IdentityFunType = () => {
   return [1, 0, 0, 0, 1, 0, 0, 0, 1];
-}
+};
 
-function projection(width: number, height: number) {
-  // Note: This matrix flips the Y axis so 0 is at the top.
+/**
+ * Projection Function
+ */
+type ProjectionFunType = (width: number, height: number) => Matrix3;
+const projection: ProjectionFunType = (width, height) => {
   return [2 / width, 0, 0, 0, -2 / height, 0, -1, 1, 1];
-}
+};
 
-function project(m: Matrix3, width: number, height: number) {
+/**
+ * Project Function
+ */
+type ProjectFunType = (m: Matrix3, width: number, height: number) => Matrix3;
+const project: ProjectFunType = (m, width, height) => {
   return multiply(m, projection(width, height));
-}
+};
 
-function translation(tx: number, ty: number) {
+/**
+ * Translation Function
+ */
+type TranslationFunType = (tx: number, ty: number) => Matrix3;
+const translation: TranslationFunType = (tx, ty) => {
   return [1, 0, 0, 0, 1, 0, tx, ty, 1];
-}
+};
 
-function translate(m: Matrix3, tx: number, ty: number) {
+/**
+ * Translate Function
+ */
+type TranslateFunType = (m: Matrix3, tx: number, ty: number) => Matrix3;
+const translate: TranslateFunType = (m, tx, ty) => {
   return multiply(m, translation(tx, ty));
-}
+};
 
-function rotation(angleInRadians: number) {
+/**
+ * Rotation Function
+ */
+type RotationFunType = (angleInRadians: number) => Matrix3;
+const rotation: RotationFunType = (angleInRadians) => {
   const c = Math.cos(angleInRadians);
   const s = Math.sin(angleInRadians);
   return [c, -s, 0, s, c, 0, 0, 0, 1];
-}
+};
 
-function rotate(m: Matrix3, angleInRadians: number) {
+/**
+ * Rotate Function
+ */
+type RotateFunType = (m: Matrix3, angleInRadians: number) => Matrix3;
+const rotate: RotateFunType = (m, angleInRadians) => {
   return multiply(m, rotation(angleInRadians));
-}
+};
 
-function scaling(sx: number, sy: number) {
+/**
+ * Scaling Function
+ */
+type ScalingFunType = (sx: number, sy: number) => Matrix3;
+const scaling: ScalingFunType = (sx, sy) => {
   return [sx, 0, 0, 0, sy, 0, 0, 0, 1];
-}
+};
 
-function scale(m: Matrix3, sx: number, sy: number) {
+/**
+ * Scale Function
+ */
+type ScaleFunType = (m: Matrix3, sx: number, sy: number) => Matrix3;
+const scale: ScaleFunType = (m, sx, sy) => {
   return multiply(m, scaling(sx, sy));
-}
+};
 
-function dot(x1: number, y1: number, x2: number, y2: number) {
+/**
+ * Dot Function
+ */
+type DotFunType = (x1: number, y1: number, x2: number, y2: number) => number;
+const dot: DotFunType = (x1, y1, x2, y2) => {
   return x1 * x2 + y1 * y2;
-}
+};
 
-function distance(x1: number, y1: number, x2: number, y2: number) {
+/**
+ * Distance Function
+ */
+type DistanceFunType = (
+  x1: number,
+  y1: number,
+  x2: number,
+  y2: number
+) => number;
+const distance: DistanceFunType = (x1, y1, x2, y2) => {
   const dx = x1 - x2;
   const dy = y1 - y2;
   return Math.sqrt(dx * dx + dy * dy);
-}
+};
 
-function normalize(x: number, y: number) {
+/**
+ * Normalize Function
+ */
+type NormalizeFunType = (x: number, y: number) => [number, number];
+const normalize: NormalizeFunType = (x, y) => {
   const l = distance(0, 0, x, y);
   if (l > 0.00001) {
     return [x / l, y / l];
   } else {
     return [0, 0];
   }
-}
+};
 
+/**
+ * Reflect Function
+ */
 // i = incident
 // n = normal
-function reflect(ix: number, iy: number, nx: number, ny: number) {
+type ReflectFunType = (
+  ix: number,
+  iy: number,
+  nx: number,
+  ny: number
+) => [number, number];
+const reflect: ReflectFunType = (ix, iy, nx, ny) => {
   // I - 2.0 * dot(N, I) * N.
   const d = dot(nx, ny, ix, iy);
   return [ix - 2 * d * nx, iy - 2 * d * ny];
-}
+};
 
-function radToDeg(r: number) {
+/**
+ * Radian To Degree
+ */
+type RadToDegFunType = (r: number) => number;
+const radToDeg: RadToDegFunType = (r) => {
   return (r * 180) / Math.PI;
-}
+};
 
-function degToRad(d: number) {
+/**
+ * Degree To Radian
+ */
+type DegToRadFunType = (d: number) => number;
+const degToRad: DegToRadFunType = (d) => {
   return (d * Math.PI) / 180;
-}
+};
 
-function transformPoint(m: Matrix3, v: [number, number]) {
+/**
+ * TransformPoint Function
+ */
+type TransformPointFunType = (
+  m: Matrix3,
+  v: [number, number]
+) => [number, number];
+const transformPoint: TransformPointFunType = (m, v) => {
   const v0 = v[0];
   const v1 = v[1];
   const d = v0 * m[0 * 3 + 2] + v1 * m[1 * 3 + 2] + m[2 * 3 + 2];
@@ -114,9 +204,13 @@ function transformPoint(m: Matrix3, v: [number, number]) {
     (v0 * m[0 * 3 + 0] + v1 * m[1 * 3 + 0] + m[2 * 3 + 0]) / d,
     (v0 * m[0 * 3 + 1] + v1 * m[1 * 3 + 1] + m[2 * 3 + 1]) / d,
   ];
-}
+};
 
-function inverse(m: Matrix3) {
+/**
+ * Inverse Function
+ */
+type InverseFunType = (m: Matrix3) => Matrix3;
+const inverse: InverseFunType = (m) => {
   const t00 = m[1 * 3 + 1] * m[2 * 3 + 2] - m[1 * 3 + 2] * m[2 * 3 + 1];
   const t10 = m[0 * 3 + 1] * m[2 * 3 + 2] - m[0 * 3 + 2] * m[2 * 3 + 1];
   const t20 = m[0 * 3 + 1] * m[1 * 3 + 2] - m[0 * 3 + 2] * m[1 * 3 + 1];
@@ -133,7 +227,11 @@ function inverse(m: Matrix3) {
     -d * (m[0 * 3 + 0] * m[2 * 3 + 1] - m[0 * 3 + 1] * m[2 * 3 + 0]),
     d * (m[0 * 3 + 0] * m[1 * 3 + 1] - m[0 * 3 + 1] * m[1 * 3 + 0]),
   ];
-}
+};
+
+/**
+ * Exports all methods
+ */
 export {
   Matrix3,
   degToRad,
