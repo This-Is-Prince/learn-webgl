f9fae16a495c714914d865adc9f56a6bd0df7440
Prince <this.is.kumar.prince@gmail.com>
2021-12-24 05:40:09 +0530

    translation rectangle revised

--- src/WebGLFundamentals/3-2D_Translation_Rotation_Scale_Matrix/2-2D_Rotation/index.ts
+++ src/WebGLFundamentals/3-2D_Translation_Rotation_Scale_Matrix/2-2D_Rotation/index.ts
@@ -1,83 +1,67 @@
-import * as dat from "dat.gui";
 import vertexShaderSource from "./shaders/vertex.vs.glsl?raw";
 import fragmentShaderSource from "./shaders/fragment.fs.glsl?raw";
+import * as dat from "dat.gui";
 
 /**
- * Dat GUI
+ * GUI
  */
-const gui = new dat.GUI();
-const rectParams = {
-  translation: { x: 0, y: 0 },
+const parameters = {
   width: 100,
-  height: 150,
-  thickness: 30,
-  rotation: { x: 0, y: 1 },
-  angle: 0,
+  height: 30,
+  translation: { x: 0, y: 0 },
 };
-
-let datGUIControllers: dat.GUIController[] = [];
+let controllers: dat.GUIController[] = [];
+const gui = new dat.GUI();
 const createControllers = (gui: dat.GUI) => {
-  datGUIControllers.forEach((controller) => {
+  controllers.forEach((controller) => {
     if (controller) {
       gui.remove(controller);
     }
   });
-
-  datGUIControllers = [];
-  datGUIControllers.push(
+  controllers = [];
+  controllers.push(
+    gui.add(parameters, "width").min(10).max(100).step(1).onChange(drawScene),
+    gui.add(parameters, "height").min(10).max(100).step(1).onChange(drawScene),
     gui
-      .add(rectParams.translation, "x")
+      .add(parameters.translation, "x")
       .min(0)
-      .max(canvas.width - rectParams.width)
+      .max(canvas.width - parameters.width)
       .step(1)
-      .onChange(() => {
-        drawScene();
-      }),
+      .onChange(drawScene),
     gui
-      .add(rectParams.translation, "y")
+      .add(parameters.translation, "y")
       .min(0)
-      .max(canvas.height - rectParams.height)
+      .max(canvas.height - parameters.height)
       .step(1)
-      .onChange(() => {
-        drawScene();
-      }),
-    gui
-      .add(rectParams, "angle")
-      .min(-Math.PI)
-      .max(Math.PI)
-      .step(0.001)
-      .onChange(() => {
-        rectParams.rotation.x = Math.cos(rectParams.angle);
-        rectParams.rotation.y = Math.sin(rectParams.angle);
-        drawScene();
-      })
+      .onChange(drawScene)
   );
 };
 
 /**
- * Create Shader
+ * Create Shader Function
  */
 type CreateShaderFunType = (
   gl: WebGLRenderingContext,
   shaderType: number,
   shaderSource: string
 ) => WebGLShader;
+
 const createShader: CreateShaderFunType = (gl, shaderType, shaderSource) => {
   const shader = gl.createShader(shaderType) as WebGLShader;
   gl.shaderSource(shader, shaderSource);
   gl.compileShader(shader);
+
   const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
   if (!success) {
     const shaderInfoLog = gl.getShaderInfoLog(shader);
     gl.deleteShader(shader);
     throw new Error(`shader is not compiled : ${shaderInfoLog}`);
   }
-
   return shader;
 };
 
 /**
- * Create Program
+ * Create Program Fun Type
  */
 type CreateProgramFunType = (
   gl: WebGLRenderingContext,
@@ -93,6 +77,7 @@ const createProgram: CreateProgramFunType = (
   gl.attachShader(program, vertexShader);
   gl.attachShader(program, fragmentShader);
   gl.linkProgram(program);
+
   const success = gl.getProgramParameter(program, gl.LINK_STATUS);
   if (!success) {
     const programInfoLog = gl.getProgramInfoLog(program);
@@ -103,82 +88,8 @@ const createProgram: CreateProgramFunType = (
 };
 
 /**
- * Set Rectangles
- */
-// type SetRectangleFunType = (
-//   gl: WebGLRenderingContext,
-//   x: number,
-//   y: number,
-//   width: number,
-//   height: number
-// ) => void;
-// const setRectangle: SetRectangleFunType = (gl, x, y, width, height) => {
-//   const x1 = x;
-//   const y1 = y;
-//   const x2 = x + width;
-//   const y2 = y + height;
-//   gl.bufferData(
-//     gl.ARRAY_BUFFER,
-//     new Float32Array([x1, y1, x2, y1, x1, y2, x1, y2, x2, y1, x2, y2]),
-//     gl.STATIC_DRAW
-//   );
-// };
-
-/**
- * Set Geometry
- */
-type SetGeometryFunType = (
-  gl: WebGLRenderingContext,
-  x: number,
-  y: number,
-  width: number,
-  height: number,
-  thickness: number
-) => void;
-
-const setGeometry: SetGeometryFunType = (
-  gl,
-  x,
-  y,
-  width,
-  height,
-  thickness
-) => {
-  const vertices2d = [
-    // left column
-    [x, y],
-    [x + thickness, y],
-    [x, y + height],
-    [x, y + height],
-    [x + thickness, y],
-    [x + thickness, y + height],
-    // top run
-    [x + thickness, y],
-    [x + width, y],
-    [x + thickness, y + thickness],
-    [x + thickness, y + thickness],
-    [x + width, y],
-    [x + width, y + thickness],
-    // middle run
-    [x + thickness, y + thickness * 2],
-    [x + width * (2 / 3), y + thickness * 2],
-    [x + thickness, y + thickness * 3],
-    [x + thickness, y + thickness * 3],
-    [x + (width * 2) / 3, y + thickness * 2],
-    [x + (width * 2) / 3, y + thickness * 3],
-  ];
-  const vertices = vertices2d.reduce((prevValue, currValue) => {
-    prevValue.push(currValue[0]);
-    prevValue.push(currValue[1]);
-    return prevValue;
-  }, []);
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
-};
-
-/**
  * Window Events
  */
-// Resize
 window.addEventListener("resize", () => {
   updateCanvasSize(canvas);
   createControllers(gui);
@@ -186,30 +97,63 @@ window.addEventListener("resize", () => {
 });
 
 /**
+ * Set Rectangle
+ */
+const setRectangle = (
+  gl: WebGLRenderingContext,
+  x: number,
+  y: number,
+  width: number,
+  height: number
+) => {
+  const x1 = x;
+  const y1 = y;
+  const x2 = x + width;
+  const y2 = y + height;
+  const rectVertices = [
+    [x1, y1],
+    [x2, y1],
+    [x1, y2],
+    [x1, y2],
+    [x2, y1],
+    [x2, y2],
+  ];
+  gl.bufferData(
+    gl.ARRAY_BUFFER,
+    new Float32Array(
+      rectVertices.reduce((prevValue, currValue) => {
+        prevValue.push(currValue[0]);
+        prevValue.push(currValue[1]);
+        return prevValue;
+      }, [])
+    ),
+    gl.STATIC_DRAW
+  );
+};
+
+/**
  * Canvas
  */
 const canvas = document.getElementById("canvas") as HTMLCanvasElement;
 
-/**
- * Update Canvas Size
- */
 const updateCanvasSize = (canvas: HTMLCanvasElement) => {
-  const { clientHeight, clientWidth } = canvas;
-  let pixelRatio = Math.min(window.devicePixelRatio, 2);
+  const { clientWidth, clientHeight } = canvas;
+  const pixelRatio = Math.min(window.devicePixelRatio, 2);
   canvas.width = (clientWidth * pixelRatio) | 0;
   canvas.height = (clientHeight * pixelRatio) | 0;
 };
-updateCanvasSize(canvas);
 
 /**
  * WebGL Rendering Context
  */
 const gl = canvas.getContext("webgl");
 if (!gl) {
-  throw new Error("webgl is not supported");
+  throw new Error(`webgl is not supported`);
 }
 
-// shaders
+/**
+ * Shaders
+ */
 const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
 const fragmentShader = createShader(
   gl,
@@ -217,58 +161,72 @@ const fragmentShader = createShader(
   fragmentShaderSource
 );
 
-// Program
+/**
+ * Program
+ */
 const program = createProgram(gl, vertexShader, fragmentShader);
 
-// a_position attribute location
+/**
+ * Location of attributes
+ */
 const positionAttributeLocation = gl.getAttribLocation(program, "a_position");
 
-// lookup uniforms
+/**
+ * Creating Buffer for attributes
+ */
+const positionBuffer = gl.createBuffer();
+
+/**
+ * Location of uniforms
+ */
 const resolutionUniformLocation = gl.getUniformLocation(
   program,
   "u_resolution"
 );
 const colorUniformLocation = gl.getUniformLocation(program, "u_color");
-const translationUniformLocation = gl.getUniformLocation(
-  program,
-  "u_translation"
-);
-const rotationUniformLocation = gl.getUniformLocation(program, "u_rotation");
 
-// Buffer for position
-const positionBuffer = gl.createBuffer();
+/**
+ * Change Canvas size
+ */
+updateCanvasSize(canvas);
 
-createControllers(gui);
-
-// bind buffer
-gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-const { height, translation, width, thickness } = rectParams;
-setGeometry(gl, translation.x, translation.y, width, height, thickness);
-
-const color = [Math.random(), Math.random(), Math.random(), 1];
-
+/**
+ * Draw Scene
+ */
 const drawScene = () => {
+  // to tell GLSL how to change clipSpace values to pixels
   gl.viewport(0, 0, canvas.width, canvas.height);
 
+  // it sets color for when you call glClear.
+  // gl.clearColor(1.0, 1.0, 0, 1.0);
   gl.clearColor(0, 0, 0, 0);
+  // it takes the color you specified above and fill canvas with that color
   gl.clear(gl.COLOR_BUFFER_BIT);
+  // use this program
   gl.useProgram(program);
+
+  /**
+   * Binding Data to attribute
+   */
   gl.enableVertexAttribArray(positionAttributeLocation);
   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  //   const { height, translation, width } = rectParams;
-  //   setRectangle(gl, translation.x, translation.y, width, height);
+  const { translation, width, height } = parameters;
+  setRectangle(gl, translation.x, translation.y, width, height);
   gl.vertexAttribPointer(positionAttributeLocation, 2, gl.FLOAT, false, 0, 0);
 
-  // set the resolution
+  /**
+   * Binding Data to uniform
+   */
   gl.uniform2f(resolutionUniformLocation, canvas.width, canvas.height);
-  const { translation, rotation } = rectParams;
-  // set the translation
-  gl.uniform2f(translationUniformLocation, translation.x, translation.y);
-  // set the rotation
-  gl.uniform2f(rotationUniformLocation, rotation.x, rotation.y);
-  // set the color
-  gl.uniform4fv(colorUniformLocation, color);
-
-  gl.drawArrays(gl.TRIANGLES, 0, 18);
+  gl.uniform4f(
+    colorUniformLocation,
+    Math.random(),
+    Math.random(),
+    Math.random(),
+    1.0
+  );
+  gl.drawArrays(gl.TRIANGLES, 0, 6);
 };
 drawScene();
+// create all controllers
+createControllers(gui);
--- src/WebGLFundamentals/3-2D_Translation_Rotation_Scale_Matrix/2-2D_Rotation/shaders/vertex.vs.glsl
+++ src/WebGLFundamentals/3-2D_Translation_Rotation_Scale_Matrix/2-2D_Rotation/shaders/vertex.vs.glsl
@@ -1,22 +1,16 @@
 attribute vec2 a_position;
 
+
 uniform vec2 u_resolution;
-uniform vec2 u_translation;
-uniform vec2 u_rotation;
 
 void main(){
-    // Rotate the Position
-    vec2 rotatedPosition = vec2(
-        a_position.x * u_rotation.y + a_position.y * u_rotation.x,
-        a_position.y * u_rotation.y - a_position.x * u_rotation.x
-    );
-    
-    // Translate the Position
-    vec2 position = rotatedPosition + u_translation;
-
-    // ClipSpace
-    vec2 zeroToOne = position / u_resolution;
+    // convert a_position to zero to one
+    vec2 zeroToOne = a_position / u_resolution;
+    // convert zeroToOne to zero to two
     vec2 zeroToTwo = zeroToOne * 2.0;
+    // convert zeroToTwo to clipSpace
     vec2 clipSpace = zeroToTwo - 1.0;
+
+    // Assigning clipSpace to gl_Position;
     gl_Position = vec4(clipSpace * vec2(1.0, -1.0), 0.0, 1.0);
 }
