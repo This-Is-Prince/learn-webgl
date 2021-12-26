273a20935e2b56b75cb02f09e8f130c6a66143ae
Prince <this.is.kumar.prince@gmail.com>
2021-12-26 21:30:23 +0530

    scaling matrix added

--- src/WebGLFundamentals/4-3D/3-3D_Camera/utils/m4.ts
+++ src/WebGLFundamentals/4-3D/3-3D_Camera/utils/m4.ts
@@ -5,9 +5,12 @@ type Mat4 = [
   [number, number, number, number]
 ];
 type Identity = () => Mat4;
+type Scaling = (sx: number, sy: number, sz: number) => Mat4;
+type xRotation = (rx: number) => Mat4;
 
 interface M4 {
   identity: Identity;
+  scaling: Scaling;
 }
 
 const m4: M4 = {
@@ -16,6 +19,14 @@ const m4: M4 = {
       [1, 0, 0, 0],
       [0, 1, 0, 0],
       [0, 0, 1, 0],
+      [0, 0, 0, 1],
+    ];
+  },
+  scaling: (sx, sy, sz) => {
+    return [
+      [sx, 0, 0, 0],
+      [0, sy, 0, 0],
+      [0, 0, sz, 0],
       [0, 0, 0, 1],
     ];
   },
