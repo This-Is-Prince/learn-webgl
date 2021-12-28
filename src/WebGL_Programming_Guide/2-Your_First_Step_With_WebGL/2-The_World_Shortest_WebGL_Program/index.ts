4f35ea2121d7b23988226aaf4ff5cadbd8ba4d42
Prince <this.is.kumar.prince@gmail.com>
2021-12-28 06:52:52 +0530

    world shortest program completed

--- src/WebGL_Programming_Guide/2-Your_First_Step_With_WebGL/2-The_World_Shortest_WebGL_Program/index.ts
+++ src/WebGL_Programming_Guide/2-Your_First_Step_With_WebGL/2-The_World_Shortest_WebGL_Program/index.ts
@@ -1,1 +1,17 @@
-console.log("hi");
+window.addEventListener("load", () => {
+  helloCanvas();
+});
+const helloCanvas = () => {
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  if (!canvas) {
+    throw new Error(`can't retrieve canvas element.`);
+  }
+  const gl = canvas.getContext("webgl");
+  if (!gl) {
+    throw new Error(`webgl is not supported in your browser.`);
+  }
+  gl.clearColor(0, 0, 0, 1);
+  gl.clearDepth(1);
+  gl.clearStencil(0);
+  gl.clear(gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT | gl.STENCIL_BUFFER_BIT);
+};
