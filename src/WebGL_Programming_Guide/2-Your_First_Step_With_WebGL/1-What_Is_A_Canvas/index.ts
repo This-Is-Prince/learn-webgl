919bc9c3e5722338bea4edb12dcf6ef4faa8a2fd
Prince <this.is.kumar.prince@gmail.com>
2021-12-28 06:28:09 +0530

    what is a canvas , completed

--- src/WebGL_Programming_Guide/2-Your_First_Step_With_WebGL/1-What_Is_A_Canvas/index.ts
+++ src/WebGL_Programming_Guide/2-Your_First_Step_With_WebGL/1-What_Is_A_Canvas/index.ts
@@ -1,1 +1,21 @@
-console.log("hi");
+window.addEventListener("load", () => {
+  main();
+});
+
+const main = () => {
+  const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+  if (!canvas) {
+    throw new Error(`Failed to retrieve the <canvas> element`);
+  }
+  const { clientHeight, clientWidth } = canvas;
+  canvas.width = clientWidth;
+  canvas.height = clientHeight;
+  console.log(canvas.width, canvas.height);
+
+  const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+  if (!ctx) {
+    throw new Error(`2d is not supported in your browser`);
+  }
+  ctx.fillStyle = "rgba(0, 0, 255, 1.0)";
+  ctx.fillRect(120, 10, 150, 150);
+};
