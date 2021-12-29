18368c15188a755b2aadf5aa122dc86b01c4d877
Prince <this.is.kumar.prince@gmail.com>
2021-12-29 19:40:57 +0530

    passing other types of information completed

--- src/WebGL_Programming_Guide/5-Using_Colors_and_Texture_Images/1-Passing_Other_Types_of_Information_to_Vertex_Shaders/index.ts
+++ src/WebGL_Programming_Guide/5-Using_Colors_and_Texture_Images/1-Passing_Other_Types_of_Information_to_Vertex_Shaders/index.ts
@@ -3,7 +3,9 @@ import fragmentShaderSource from "./shaders/fragment.fs.glsl?raw";
 import { createProgram, createShader } from "../utils";
 
 window.addEventListener("load", () => {
-  multiAttributeSize();
+  // multiAttributeSize();
+  // multiAttributeSize_Interleaved();
+  multiAttributeColor();
 });
 
 const updateCanvasResolution = (
@@ -15,7 +17,7 @@ const updateCanvasResolution = (
   canvas.height = (clientHeight * pixelRatio) | 0;
 };
 
-const multiAttributeSize = () => {
+const multiAttributeColor = () => {
   /**
    * Canvas
    */
@@ -48,37 +50,163 @@ const multiAttributeSize = () => {
   /**
    * a_Position Location
    */
+  const vertexSizeBuffer = gl.createBuffer();
   const a_Position = gl.getAttribLocation(program, "a_Position");
-  const positionBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  const vertices = [0, 0.5, -0.5, -0.5, 0.5, -0.5];
-  gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexSizeBuffer);
+  const verticesSizes = new Float32Array([
+    0, 0.5, 1.0, 0.0, 0.0, -0.5, -0.5, 0.0, 1.0, 0.0, 0.5, -0.5, 0.0, 0.0, 1.0,
+  ]);
+  gl.bufferData(gl.ARRAY_BUFFER, verticesSizes, gl.STATIC_DRAW);
 
   /**
    * a_PointSize Location
    */
-  const a_PointSize = gl.getAttribLocation(program, "a_PointSize");
-  const pointBuffer = gl.createBuffer();
-  gl.bindBuffer(gl.ARRAY_BUFFER, pointBuffer);
-  const verticesSize = [10, 20, 35];
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array(verticesSize),
-    gl.STATIC_DRAW
-  );
+  const a_Color = gl.getAttribLocation(program, "a_Color");
 
   gl.useProgram(program);
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.width);
   gl.clearColor(0, 0, 0, 1);
   gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+  gl.bindBuffer(gl.ARRAY_BUFFER, vertexSizeBuffer);
+  const FSIZE = verticesSizes.BYTES_PER_ELEMENT;
+
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 5, 0);
   gl.enableVertexAttribArray(a_Position);
 
-  gl.bindBuffer(gl.ARRAY_BUFFER, pointBuffer);
-  gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, 0, 0);
-  gl.enableVertexAttribArray(a_PointSize);
+  gl.vertexAttribPointer(a_Color, 3, gl.FLOAT, false, FSIZE * 5, FSIZE * 2);
+  gl.enableVertexAttribArray(a_Color);
 
-  gl.drawArrays(gl.POINTS, 0, verticesSize.length);
+  gl.drawArrays(gl.TRIANGLES, 0, verticesSizes.length / 5);
 };
+
+// const multiAttributeSize_Interleaved = () => {
+//   /**
+//    * Canvas
+//    */
+//   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+//   updateCanvasResolution(canvas, Math.min(window.devicePixelRatio, 2));
+
+//   /**
+//    * WebGL Rendering Context
+//    */
+//   const gl = canvas.getContext("webgl") as WebGLRenderingContext;
+//   if (!gl) {
+//     throw new Error(`webgl is not supported`);
+//   }
+
+//   /**
+//    * Shaders
+//    */
+//   const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+//   const fragmentShader = createShader(
+//     gl,
+//     gl.FRAGMENT_SHADER,
+//     fragmentShaderSource
+//   );
+
+//   /**
+//    * Program
+//    */
+//   const program = createProgram(gl, vertexShader, fragmentShader);
+
+//   /**
+//    * a_Position Location
+//    */
+//   const vertexSizeBuffer = gl.createBuffer();
+//   const a_Position = gl.getAttribLocation(program, "a_Position");
+//   gl.bindBuffer(gl.ARRAY_BUFFER, vertexSizeBuffer);
+//   const verticesSizes = new Float32Array([
+//     0, 0.5, 10, -0.5, -0.5, 20, 0.5, -0.5, 30,
+//   ]);
+//   gl.bufferData(gl.ARRAY_BUFFER, verticesSizes, gl.STATIC_DRAW);
+
+//   /**
+//    * a_PointSize Location
+//    */
+//   const a_PointSize = gl.getAttribLocation(program, "a_PointSize");
+
+//   gl.useProgram(program);
+//   gl.viewport(0, 0, gl.canvas.width, gl.canvas.width);
+//   gl.clearColor(0, 0, 0, 1);
+//   gl.clear(gl.COLOR_BUFFER_BIT);
+
+//   gl.bindBuffer(gl.ARRAY_BUFFER, vertexSizeBuffer);
+//   const FSIZE = verticesSizes.BYTES_PER_ELEMENT;
+
+//   gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 3, 0);
+//   gl.enableVertexAttribArray(a_Position);
+
+//   gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, FSIZE * 3, FSIZE * 2);
+//   gl.enableVertexAttribArray(a_PointSize);
+
+//   gl.drawArrays(gl.POINTS, 0, verticesSizes.length / 3);
+// };
+
+// const multiAttributeSize = () => {
+//   /**
+//    * Canvas
+//    */
+//   const canvas = document.getElementById("canvas") as HTMLCanvasElement;
+//   updateCanvasResolution(canvas, Math.min(window.devicePixelRatio, 2));
+
+//   /**
+//    * WebGL Rendering Context
+//    */
+//   const gl = canvas.getContext("webgl") as WebGLRenderingContext;
+//   if (!gl) {
+//     throw new Error(`webgl is not supported`);
+//   }
+
+//   /**
+//    * Shaders
+//    */
+//   const vertexShader = createShader(gl, gl.VERTEX_SHADER, vertexShaderSource);
+//   const fragmentShader = createShader(
+//     gl,
+//     gl.FRAGMENT_SHADER,
+//     fragmentShaderSource
+//   );
+
+//   /**
+//    * Program
+//    */
+//   const program = createProgram(gl, vertexShader, fragmentShader);
+
+//   /**
+//    * a_Position Location
+//    */
+//   const a_Position = gl.getAttribLocation(program, "a_Position");
+//   const positionBuffer = gl.createBuffer();
+//   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+//   const vertices = [0, 0.5, -0.5, -0.5, 0.5, -0.5];
+//   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(vertices), gl.STATIC_DRAW);
+
+//   /**
+//    * a_PointSize Location
+//    */
+//   const a_PointSize = gl.getAttribLocation(program, "a_PointSize");
+//   const pointBuffer = gl.createBuffer();
+//   gl.bindBuffer(gl.ARRAY_BUFFER, pointBuffer);
+//   const verticesSize = [10, 20, 35];
+//   gl.bufferData(
+//     gl.ARRAY_BUFFER,
+//     new Float32Array(verticesSize),
+//     gl.STATIC_DRAW
+//   );
+
+//   gl.useProgram(program);
+//   gl.viewport(0, 0, gl.canvas.width, gl.canvas.width);
+//   gl.clearColor(0, 0, 0, 1);
+//   gl.clear(gl.COLOR_BUFFER_BIT);
+
+//   gl.bindBuffer(gl.ARRAY_BUFFER, positionBuffer);
+//   gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+//   gl.enableVertexAttribArray(a_Position);
+
+//   gl.bindBuffer(gl.ARRAY_BUFFER, pointBuffer);
+//   gl.vertexAttribPointer(a_PointSize, 1, gl.FLOAT, false, 0, 0);
+//   gl.enableVertexAttribArray(a_PointSize);
+
+//   gl.drawArrays(gl.POINTS, 0, verticesSize.length);
+// };
--- src/WebGL_Programming_Guide/5-Using_Colors_and_Texture_Images/1-Passing_Other_Types_of_Information_to_Vertex_Shaders/shaders/vertex.vs.glsl
+++ src/WebGL_Programming_Guide/5-Using_Colors_and_Texture_Images/1-Passing_Other_Types_of_Information_to_Vertex_Shaders/shaders/vertex.vs.glsl
@@ -1,8 +1,33 @@
-attribute vec4 a_Position;
-attribute float a_PointSize;
+// MultiAttributeSize
 
+// attribute vec4 a_Position;
+// attribute float a_PointSize;
+
+
+// void main(){
+//     gl_Position = a_Position;
+//     gl_PointSize = a_PointSize;
+// }
+
+// MultiAttributeSize_Interleaved;
+
+// attribute vec4 a_Position;
+// attribute float a_PointSize;
+
+// void main(){
+//     gl_Position = a_Position;
+//     gl_PointSize = a_PointSize;
+// }
+
+// MultiAttributeColor
+
+attribute vec4 a_Position;
+attribute vec4 a_Color;
+
+varying vec4 v_Color;
 
 void main(){
     gl_Position = a_Position;
-    gl_PointSize = a_PointSize;
+    
+    v_Color = a_Color;
 }
