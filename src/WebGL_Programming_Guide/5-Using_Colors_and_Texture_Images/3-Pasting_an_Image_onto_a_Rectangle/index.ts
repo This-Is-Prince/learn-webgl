783b59782803ca99de51436e46479745a09bad56
Prince <this.is.kumar.prince@gmail.com>
2021-12-30 09:20:02 +0530

    image displayed on rectangle

--- src/WebGL_Programming_Guide/5-Using_Colors_and_Texture_Images/3-Pasting_an_Image_onto_a_Rectangle/index.ts
+++ src/WebGL_Programming_Guide/5-Using_Colors_and_Texture_Images/3-Pasting_an_Image_onto_a_Rectangle/index.ts
@@ -48,19 +48,64 @@ const pastingAnImage = () => {
   /**
    * a_Position Location
    */
-  const vertices = new Float32Array([0, 0.5, -0.5, -0.5, 0.5, -0.5]);
+  const vertices = new Float32Array([
+    -0.5, 0.5, 0.0, 1.0, -0.5, -0.5, 0.0, 0.0, 0.5, 0.5, 1.0, 1.0, 0.5, -0.5,
+    1.0, 0.0,
+  ]);
   const buffer = gl.createBuffer();
   const a_Position = gl.getAttribLocation(program, "a_Position");
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, vertices, gl.STATIC_DRAW);
 
+  /**
+   * a_TexCoord Location
+   */
+  const a_TexCoord = gl.getAttribLocation(program, "a_TexCoord");
+
+  /**
+   * Texture
+   */
+  // creating a texture object
+  const texture = gl.createTexture();
+  // get the storage location of the u_Sampler
+  const u_Sampler = gl.getUniformLocation(program, "u_Sampler");
+  // create an image object
+  const image = new Image();
+  // tell the browser to load an image
+  image.src = "../../../../static/images/dragon.jpg";
+  // register the event handler to be called on loading an image
+  image.addEventListener("load", () => {
+    // Flip the image's y axis
+    gl.pixelStorei(gl.UNPACK_FLIP_Y_WEBGL, 1);
+    // Enable the texture unit 0
+    gl.activeTexture(gl.TEXTURE0);
+    // Bind the texture object to the target
+    gl.bindTexture(gl.TEXTURE_2D, texture);
+    // Set the texture parameters
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MIN_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_MAG_FILTER, gl.NEAREST);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_S, gl.CLAMP_TO_EDGE);
+    gl.texParameteri(gl.TEXTURE_2D, gl.TEXTURE_WRAP_T, gl.CLAMP_TO_EDGE);
+    // Set the texture image
+    gl.texImage2D(gl.TEXTURE_2D, 0, gl.RGBA, gl.RGBA, gl.UNSIGNED_BYTE, image);
+    // Set the texture unit 0 to the sampler
+    gl.uniform1i(u_Sampler, 0);
+
+    // set clear color
+    gl.clearColor(0, 0, 0, 1);
+    // clear the canvas with above specified color
+    gl.clear(gl.COLOR_BUFFER_BIT);
+    // Draw a rectangle
+    gl.drawArrays(gl.TRIANGLE_STRIP, 0, 4);
+  });
+
   gl.viewport(0, 0, gl.canvas.width, gl.canvas.height);
   gl.useProgram(program);
-  gl.clearColor(0, 0, 0, 1);
-  gl.clear(gl.COLOR_BUFFER_BIT);
 
-  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, 0, 0);
+  const FSIZE = vertices.BYTES_PER_ELEMENT;
+  gl.vertexAttribPointer(a_Position, 2, gl.FLOAT, false, FSIZE * 4, 0);
   gl.enableVertexAttribArray(a_Position);
 
-  gl.drawArrays(gl.TRIANGLES, 0, 3);
+  gl.vertexAttribPointer(a_TexCoord, 2, gl.FLOAT, false, FSIZE * 4, FSIZE * 2);
+  gl.enableVertexAttribArray(a_TexCoord);
 };
