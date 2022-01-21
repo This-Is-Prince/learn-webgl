eaee0b22807b0d991cbe447199a061a1b1ee973b
Prince <this.is.kumar.prince@gmail.com>
2022-01-21 20:19:10 +0530

    matrix4 completed

--- src/Fun_With_WebGL_2.0/Lesson_005/app.ts
+++ src/Fun_With_WebGL_2.0/Lesson_005/app.ts
@@ -6,6 +6,7 @@ import { RenderLoop } from "./utils/RenderLoop";
 import { WebGL2Context } from "./lib";
 import { Modal } from "./Modal/Modal";
 import { Primitives } from "./primitives/Primitives";
+import { Matrix4 } from "./utils/Math";
 
 window.addEventListener("load", () => {
   /**
