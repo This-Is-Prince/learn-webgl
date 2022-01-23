4a250b37dc53b1a9bbab529d0346002a24ab341a
Prince <this.is.kumar.prince@gmail.com>
2022-01-23 18:34:28 +0530

    lesson 6 GL basic functions done

---
+++ src/Fun_With_WebGL_2.0/Lesson_006/lib.d.ts
@@ -0,0 +1,46 @@
+interface MeshData {
+  drawMode: number;
+  vao: WebGLVertexArrayObject;
+
+  verticesBuffer?: WebGLBuffer;
+  vertexComponentLen?: number;
+  vertexCount?: number;
+
+  normalsBuffer?: WebGLBuffer;
+
+  uvBuffer?: WebGLBuffer;
+
+  indicesBuffer?: WebGLBuffer;
+  indexCount?: number;
+}
+interface MeshesData {
+  [key: string]: MeshData;
+}
+
+interface WebGL2Context extends WebGL2RenderingContext {
+  fClearScreen: () => WebGL2Context;
+  fSetSize: (w: number, h: number) => WebGL2Context;
+  fCreateArrayBuffer: (
+    srcData: BufferSource,
+    isStatic?: boolean
+  ) => WebGLBuffer;
+  fCreateMeshVAO: (
+    name: string,
+    indices?: number[] | null,
+    vertices?: number[] | null,
+    normals?: number[] | null,
+    uv?: number[] | null
+  ) => MeshData;
+  meshesData: MeshesData;
+}
+
+interface Attribute {
+  name: string;
+  location: number;
+}
+interface Attributes {
+  position: Attribute;
+  normal: Attribute;
+  uv: Attribute;
+}
+export { Attribute, Attributes, MeshData, MeshesData, WebGL2Context };
